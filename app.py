c171ee65a2ca5753fc2ad1313866bedeb4e7d61e
benoniokaikoi <derhain@gmail.com>
2025-06-02 12:05:43 +0000

    Deploy cert_flask_backend to Render

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ def home():
     return jsonify(message="Hello from Flask backend!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
